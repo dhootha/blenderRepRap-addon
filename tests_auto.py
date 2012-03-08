ba64e22304f57c7896b27a0971c1c58faaf3ca3d
Radoslav Borisov <radoslav.borisov@gmail.com>
2012-03-08 16:49:17 +0200

    Added USB auto recognition and X, Y, Z, E verification. Some functions cleaned up.

--- tests_auto.py
+++ tests_auto.py
@@ -1,110 +1,146 @@
 import sys
 # we should consider adding pyserial into blender path and make tests !
-PYSERIAL_PATH = '/usr/local/lib/python3.2/dist-packages/'
+PYSERIAL_PATH = "/usr/local/lib/python3.2/dist-packages/"
+DEBUG_COMMUNICATION = False
 
 def checkpaths():
-    """checks if PySerial already defined in sys.path()"""
-    """if defined do nothing, else add path to blender"""
+    """ checks if PySerial already defined in sys.path()
+
+    if already defined in path, prints message in console,
+    else adds path to blender path.
+    
+    """
     if (PYSERIAL_PATH in sys.path):
-        print('PySerial path already defined.')
+        print("PySerial path already defined.")
     else:
-        sys.path.append(PYSERIAL_PATH)
-        print('PySerial added to path.')
+        sys.path.append(PYSERIAL_PATH)       # print("PySerial added to path.")
         
 checkpaths()    # adds PySerial path to blender
 
 from serial import Serial
 import glob
+import os
 
 def scanserial():
-    """scan for available ports. return a list of device names."""
+    """scan for available ports. 
+    return a list of device names. """
     baselist=[]
-#       if os.name=="nt":
-#       try:
-#           key=_winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE,"HARDWARE\\DEVICEMAP\\SERIALCOMM")
-#               i=0
-#               while(1):
-#                   baselist+=[_winreg.EnumValue(key,i)[1]]
-#                   i+=1
-#       except:
-#           pass
-
+    if os.name == "nt" :
+        pass
+#        import winreg
+#        try:
+#            key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE,"HARDWARE\\DEVICEMAP\\SERIALCOMM")
+#            i = 0
+#            while(1):
+#                baselist += [winreg.EnumValue(key,i)[1]]
+#                i += 1
+#        except:
+#            pass
+    if os.name == "posix" :
+        pass
+    
     return baselist+glob.glob('/dev/ttyUSB*') + glob.glob('/dev/ttyACM*') + glob.glob("/dev/tty.*") + glob.glob("/dev/cu.*") + glob.glob("/dev/rfcomm*")
 
 def testport():        
-    """Test found available port from scanserial() function and returns 'port name' or 'none' '"""
-    ok = 'none'
+    """ Test found available port from scanserial() function 
+    returns 'port name' or 'none' """
+    ok = "none"
     
     port_name = scanserial()
-    print(port_name)
-    print('Number of ports found: ', len(port_name))
-    
-    num = len(port_name)
-    print('Found port numbers: ', num)
-    
-    for port_name[num-1] in port_name:
+    if DEBUG_COMMUNICATION:
+        print(port_name)
+        print("Number of ports found: ", len(port_name))
+    else:
+        pass
         
-        printer = Serial(port_name[num-1], 115200, timeout = 5)
-        answer = printer.readline().strip()
-        printer.close()
-        print(answer.decode('ascii'))
-        if ('start' in answer.decode('ascii')):
-            ok = port_name[num-1]
-            print('OK port is: ', ok)
-        else:
-            pass
+    num = len(port_name)
     
-    if ok is not 'none':            
+    try:    
+        for port_name[num-1] in port_name:
+            printer = Serial(port_name[num-1], 115200, timeout = 10)
+         
+            answer = printer.readline().strip()
+            printer.close()
+            print(answer.decode('ascii'))
+            if ("start" in answer.decode('ascii')):
+                ok = port_name[num-1]
+                if DEBUG_COMMUNICATION:
+                    print("OK port is: ", ok)
+                else:
+                    pass
+            else:
+                pass
+    
+    except:
+        print("Something happend, don't know yet.")
+           
+    if ok is not "none":            
         return ok
     else:
         return ok
-        print( 'Port not found.' )
-        
-#    Traceback (most recent call last):
-#    File "/usr/local/lib/python3.2/dist-packages/serial/serialposix.py", line 275, in open
-#    self.fd = os.open(self.portstr, os.O_RDWR|os.O_NOCTTY|os.O_NONBLOCK)
-#    OSError: [Errno 16] Device or resource busy: '/dev/ttyACM0'
-#
-#    During handling of the above exception, another exception occurred:
-#
-#    Traceback (most recent call last):
-#    File "/Text", line 76, in <module>
-#    File "/Text", line 47, in testport
-#    File "/usr/local/lib/python3.2/dist-packages/serial/serialutil.py", line 261, in __init__
-#    self.open()
-#    File "/usr/local/lib/python3.2/dist-packages/serial/serialposix.py", line 278, in open
-#    raise SerialException("could not open port %s: %s" % (self._port, msg))
-#    serial.serialutil.SerialException: could not open port /dev/ttyACM0: [Errno 16] Device or resource busy: '/dev/ttyACM0'
+        print("Port not found.")
 
-        
+def check_axis(axis):
+    """ Checks input parameters for the movement
+    Returns True or False. """
+    allowed = ["X","Y","Z","E"]
     
+    if DEBUG_COMMUNICATION:
+        print(allowed)
+        print("Axis is: ")
+        print(axis)
+    else:
+        pass    
+     
+    if axis in allowed:
+        return True
+    else:
+        return False
+        
 def move(axis, direction, value, speed):
-    """Moves given 'axis' in '+' or '-' 'direction' with 'value' and 'speed'"""
-    axis = str(axis)    # X, Y, Z, E
+    """ Moves given 'axis' in '+' or '-' 'direction' with 'value' and 'speed'
+    
+    
+    
+    """
+    axis_work = "none"
+    
+    if check_axis(axis):
+        axis_work = axis
+    else:
+        print("Axis definition Error. Please enter \"X\", \"Y\", \"Z\", \"E\". ")
+        
+    #axis = str(axis)   # X, Y, Z, E
     direction = str(direction)  # '+' or '-'
     value = str(value)  # relative move 
     speed = str(speed)  # set speed of movement from 0 to 3000
 
-    word = 'G1 ' + axis + direction + value + ' F' + speed + '\r\n' 
-    print( word.strip() )
-    
-    return word.encode('ascii')    
+    if axis_work is not "none":
+        word = "G1 " + axis_work + direction + value + " F" + speed + "\r\n"
+        return word.encode('ascii') 
+        print( word.strip() )
+    else:
+        pass
 
 port = testport()
 baud = 115200
 
 print(port)
-if ('ACM' in port):
+if ("ACM" in port):
     printer = Serial(port, baud, timeout = 5)
     print(printer.readline().strip().decode('ascii'))
     word = 'G91\r\n'
     printer.write(word.encode('ascii'))
     word = move('X', '-', 15, 600)
-    printer.write(word)
-else: pass
+    try:
+        printer.write(word)
+    except:
+        print("No word to write.")
+else: 
+    pass
 
-if ('none' not in port):
+if ("none" not in port):
     if printer.isOpen():
         printer.close()
 else: pass
-print('That\'s all folks.')
+print("That\'s all folks.")
